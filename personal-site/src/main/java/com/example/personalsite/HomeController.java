876bfe3a4270796cb7e0556a9b0fe6dcf3c12dd2
nintreesssss <nintreesssss@gmail.com>
2026-01-28 16:42:49 +0800

    完成了index.html，有基础的卡片简介框架

--- personal-site/src/main/java/com/example/personalsite/HomeController.java
+++ personal-site/src/main/java/com/example/personalsite/HomeController.java
@@ -2,14 +2,20 @@ package com.example.personalsite;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
-    public String home() {
-        return "<h1>Hello World!";
+
+    public String home(Model model) {
+        model.addAttribute("name","nintrees");
+        model.addAttribute("bio","MUST CS本科在读");
+        model.addAttribute("github", "https://github.com/nintreesssss");
+        model.addAttribute("x", "https://twitter.com/nintrees"); 
+        model.addAttribute("email", "nintreesssss@gmail.com");
+
+        return "index"; //渲染src/main/resources/templates/index.html
     }
 }
